5390e14a74c008f31097da33361cd55cf15b25a1
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-07-20 19:30:25 +0530

    Implemented Home UI screen

--- chittchat_flutter/lib/src/feature/home/presentation/home_screen.dart
+++ chittchat_flutter/lib/src/feature/home/presentation/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -8,18 +9,50 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChittChat'),
+        title: const Text('ChitChat 💬'),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 1.sw > 1.sh ? 26.sp : 86.sp,
+        ),
         backgroundColor: Colors.blue,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(18.0),
+      body: Container(
+        padding: const EdgeInsets.all(18.0),
+        width: double.infinity,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'Lets\' start chatting!!!',
+              'Lets\' start Chitchat!!!',
               style: TextStyle(
-                fontSize: 32,
+                fontSize: 1.sw > 1.sh ? 32.sp : 92.sp,
               ),
+            ),
+            12.verticalSpace,
+            SizedBox(
+              width: 0.6.sw,
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter Code',
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            12.verticalSpace,
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Join Now'),
+            ),
+            18.verticalSpace,
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Create New Chitchat'),
             ),
           ],
         ),
